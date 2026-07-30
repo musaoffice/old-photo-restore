b0cd2d72e03f9082ba4c896369682990042601d3
musaoffice <musaaoffice@gmail.com>
2026-07-30 12:16:33 +0500

    Add PaddleScript component to layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -17,7 +17,8 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" className="h-full w-full" data-theme={theme}>
       <body className={`${font.className} h-full w-full flex flex-col antialiased bg-bg-page text-primary-text overflow-hidden`}>
-        <Providers>
+         <PaddleScript />
+         <Providers>
           <Navbar />
           <div className="flex-1 flex flex-col overflow-hidden min-h-0">
             {children}
